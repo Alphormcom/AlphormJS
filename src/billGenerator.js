8ba59a26315cc26de03f08c77222c1b4c8f6b41c
Mohamed Moughamir <mohamed@alphorm.com>
2018-07-23 11:01:42 +0100

    Replaced PdfKit avec PdfMake

--- src/billGenerator.js
+++ src/billGenerator.js
@@ -1,23 +1,38 @@
-import PDFDocument from 'pdfkit'
-import * as fs from 'fs'
-
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+pdfMake.vfs = pdfFonts.pdfMake.vfs
 export const billGenerator = (content, destination, attributes) => {
-  let doc = new PDFDocument({
-    Size: attributes.size,
-    Title: attributes.title,
-    Author: attributes.author,
-    Subject: attributes.subject,
-    CreationDate: attributes.creationDate,
-    ModDate: attributes.creationDate
-  })
+  let docDefinition = {
+    pageSize: attributes.size,
+    defaultStyle: {
+      font: 'OpenSans'
+    }
+  }
+  // let doc = new PDFDocument({
+  //   Size: attributes.size,
+  //   Title: attributes.title,
+  //   Author: attributes.author,
+  //   Subject: attributes.subject,
+  //   CreationDate: attributes.creationDate,
+  //   ModDate: attributes.creationDate
+  // })
   // pdfMake.createPdf(content).download(`${destination}.pdf`)
-  doc.pipe(fs.createWriteStream(`/bill/${destination}.pdf`))
-  doc.font('Fira Code')
-    .fontSize(18)
-    .text(content.title, 100, 100)
-  doc.addPage()
-  doc.rect(100, 120, 250, 250)
-  doc.fillColor('red')
+  // doc.pipe(fs.createWriteStream(`/bill/${destination}.pdf`))
+  // doc.font('Fira Code')
+  //   .fontSize(18)
+  //   .text(content.title, 100, 100)
+  // doc.addPage()
+  // doc.rect(100, 120, 250, 250)
+  // doc.fillColor('red')
 
-  doc.end()
+  // doc.end()
+  if (attributes.do === 'download') {
+    pdfMake.createPdf(docDefinition).download(`${destination}.pdf`)
+  } else if (attributes.do === 'view') {
+    pdfMake.createPdf(docDefinition).open(`${destination}.pdf`)
+  } else if (attributes.do === 'print') {
+    pdfMake.createPdf(docDefinition).print(`${destination}.pdf`)
+  } else {
+    console.error('Method undefined')
+  }
 }
