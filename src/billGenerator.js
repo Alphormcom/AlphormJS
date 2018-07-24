84a4c40897b2f0943e79ad3672fb0bf56126cad0
Mohamed Moughamir <mohamed@alphorm.com>
2018-07-24 17:20:26 +0100

    Add Items iteration

--- src/billGenerator.js
+++ src/billGenerator.js
@@ -1,11 +1,52 @@
-import pdfMake from 'pdfmake/build/pdfmake'
+﻿import pdfMake from 'pdfmake/build/pdfmake'
 import pdfFonts from 'pdfmake/build/vfs_fonts'
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 export const billGenerator = (content, destination, attributes) => {
+  let docBlob = null
   let docDefinition = {
-    pageSize: attributes.size,
-    defaultStyle: {
-      font: 'OpenSans'
+    pageSize: 'A4',
+    pageOrientation: 'portrait',
+    pageMargins: [20, 60, 20, 40],
+    footer: {text: attributes.footer, style: 'footer'},
+    content: [
+      {
+        style: 'table',
+        table: {
+          headerRows: 1,
+          widths: ['*', 35, 60, 100],
+          body: [
+            [{ text: 'Article', style: 'tableHeader' }, { text: 'Qté', style: 'tableHeader' }, { text: 'P.U HT (€)', style: 'tableHeader' }, { text: 'Montant HT (€)', style: 'tableHeader' }],
+            [{
+              ul: content.items.map(item => item.description)
+            },
+            content.items.map(item => item.quatity ? item.quatity : '1'),
+            content.items.map(item => item.PriceExcludingVat),
+            content.items.map(item => item.PriceExcludingVat * (item.quatity ? item.quatity : '1'))
+            ],
+            [{ text: '', border: [false, false, false, false] }, { colSpan: 2, rowSpan: 1, text: 'Total HT', style: 'tableHeader' }, '', parseFloat(content.totalPrice).toFixed(2)],
+            [{ text: '', border: [false, false, false, false] }, { colSpan: 2, rowSpan: 1, text: 'Total TVA (20%)', style: 'tableHeader' }, '', parseFloat(content.totalPrice).toFixed(2)],
+            [{ text: '', border: [false, false, false, false] }, { colSpan: 2, rowSpan: 1, text: 'Total TTC (€)', style: 'tableHeader' }, '', parseFloat(content.totalPrice).toFixed(2)]
+          ]
+        }
+      }
+
+    ],
+
+    styles: {
+      tableHeader: {
+        fillColor: '#dfe6e9',
+        fontSize: 12,
+        bold: true
+      },
+      table: {
+        margin: [15, 15, 15, 15]
+      },
+      footer: {
+        fontSize: 7,
+        margin: [60, 0, 60, 0],
+        alignment: 'center'
+
+      }
     }
   }
   // let doc = new PDFDocument({
@@ -29,10 +70,16 @@ export const billGenerator = (content, destination, attributes) => {
   if (attributes.do === 'download') {
     pdfMake.createPdf(docDefinition).download(`${destination}.pdf`)
   } else if (attributes.do === 'view') {
-    pdfMake.createPdf(docDefinition).open(`${destination}.pdf`)
+    pdfMake.createPdf(docDefinition).open({}, window)
   } else if (attributes.do === 'print') {
     pdfMake.createPdf(docDefinition).print(`${destination}.pdf`)
+  } else if (attributes.do === 'buffer') {
+    pdfMake.createPdf(docDefinition).getDataUrl(function (result) {
+      docBlob = result
+      console.log(docBlob)
+    })
   } else {
     console.error('Method undefined')
   }
+  return docBlob
 }
--- test/bill.js
+++ test/bill.js
@@ -1,41 +1,54 @@
-import billGenerator from '../src/billGenerator'
+﻿import {billGenerator} from '../src/billGenerator'
 
 const attributes = {
   size: 'A4', // LEGAL....
   title: 'Facture-XYZ',
   author: 'Alphard Technology',
   subject: 'Facture',
-  creationDate: '2015-09-31'
+  creationDate: '2015-09-31',
+  do: 'buffer',
+  footer: `Alphard Technologies 9 Charles Fourier, Evry 91030 | Tél : +33 (0)1 77 62 45 80 | E-mail: contact@alphorm.com | www.alphorm.com
+  Siret : 53046978200011 - Naf : 6203Z - N° TVA intracom : FR04530469782 - Capital : 21 000,00 € - N° Déclaration : 11 91 07268 91`
 }
 const content = {
-  title: attributes.title,
-  orderNumber: 'FA-2015-09',
-  issueDate: attributes.creationDate,
-  paymentMethod: 'Paypal',
-  User: {
-    firstname: 'Adiss',
-    lastname: 'Abiba',
-    email: 'zeus@pharaon.com',
-    phone: '+1 (665) 225 2254 252'
+  'OrderNumber': 'FA-20187',
+  'IssueDate': '2018-07-23T00:00:00',
+  'PaymentMethod': 'SMS',
+  'User': {
+    'LastName': 'Andrei',
+    'FirstName': 'Nicora',
+    'Email': 'andrei@alphorm.com',
+    'Phone': null
   },
-  billlingAdress: {
-    VatNumber: 'VAT-25452',
-    Company: 'OmniZya',
-    AddressLine1: '10 rue 2, bloc E',
-    AddressLine2: 'Laglita, SidiNaimi',
-    Zipcode: '27182',
-    City: 'Bouskoura',
-    State: 'Grand Casablanca',
-    CountryId: 'MA'
+  'BillingAddress': {
+    'AddressId': null,
+    'Company': 'Alphorm Dev. ',
+    'VatNumber': '145',
+    'AddressLine1': 'Dar lol test',
+    'AddressLine2': null,
+    'City': 'RABAT',
+    'CountryId': 0,
+    'Country': null,
+    'PostalCode': null,
+    'State': null,
+    'AddressTypeId': 0
   },
-  PriceExcludingVat: '15.9',
-  VatAmount: '3.18',
-  totalPrice: this.PriceExcludingVat + this.VatAmount,
-  currency: '€',
-  items: [
+  'PriceExcludingVat': 0,
+  'VatAmount': 0,
+  'TotalPrice': 400,
+  'Currency': 'EUR',
+  'Items': [
     {
-      description: 'LifeTime subscription',
-      PriceExcludingVat: this.PriceExcludingVat
+      'Description': 'VMware vSphere 5 : Savoir installer, configurer et administrer',
+      'PriceExcludingVat': 176,
+      'VatAmount': 0,
+      'TotalPrice': 176
+    },
+    {
+      'Description': 'VMware Mirage 3.6 : Savoir installer, configurer et administrer',
+      'PriceExcludingVat': 144,
+      'VatAmount': 0,
+      'TotalPrice': 144
     }
   ]
 }
